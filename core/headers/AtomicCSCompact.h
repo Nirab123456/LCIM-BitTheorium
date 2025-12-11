35a3d2a5536659171a0a3900f961010c74c46206
nirab123456 <rifatulislam210@gmail.com>
2025-12-11 14:40:24 -0500

    AtomicCSCompact-> added

--- core/headers/AtomicCSCompact.h
+++ core/headers/AtomicCSCompact.h
@@ -43,20 +43,33 @@ namespace AtomicCSCompact
     template<typename T>
     void InitAnyView(std::atomic<T>*& dataptr, size_t N, size_t& cn);
 
+
+    template <
+        size_t VALIB,
+        size_t STRLB,
+        size_t CLKB,
+        typename OUT = uint64_t
+    >
+
+    struct BitPacker;
     class ACBCompact4_t
     {
     private:
         size_t n_ = 0;
         std::atomic<uint32_t>* data_ = nullptr;
     public:
-       ACBCompact4_t() = default;
-       ~ACBCompact4_t()
-       {
+        ACBCompact4_t() = default;
+        ~ACBCompact4_t()
+        {
             FreeAll(data_);
-       } 
-    
-        struct FieldView4_t;
+        } 
 
-        
+        struct FieldView4_t;
+        size_t size() const { return n_;}
+
+        static constexpr uint32_t C4TMask = 0xfu;
+        static constexpr uint32_t CLKMask = 0xffffu;
+
+
     };
 }
--- core/programes/AtomicCSCompact.cpp
+++ core/programes/AtomicCSCompact.cpp
@@ -26,7 +26,78 @@ namespace AtomicCSCompact
     }
 
 
+    template<typename OUT = uint32_t, typename VAL = uint8_t, typename STRL = uint4_tt, typename CLK = uint8_t>
+    static OUT PackAny(VAL value, VAL inv_value, STRL st, STRL rel, CLK clk)
+    {
+        OUT r = 0;
+        r |= static_cast<OUT>(clk);
+        r <<= sizeof(CLK);
+        r |= static_cast<OUT>(st);
+        r <<= sizeof(STRL);
+        r |= static_cast<OUT>(rel);
+        r <<= sizeof(STRL);
+        r |= static_cast<OUT>(inv_value);
+        r <<= sizeof(VAL);
+        r |= static_cast<OUT>(value);
+        return r;
+    }
 
+    template <
+        size_t VALIB,
+        size_t STRLB,
+        size_t CLKB,
+        typename OUT = uint64_t
+    >
+    struct BitPacker{
+        static_assert(std::is_unsigned_v<OUT>, "OUT must be an unsigned integral");
+        static constexpr size_t TotatBytes = VALIB + VALIB + STRLB + STRLB + CLKB;
+        static_assert(TotatBytes <= sizeof(OUT)*8, "Total bits/bytes are greater than return size");
+
+        static constexpr OUT ValIMask = (VALIB == 0 ? OUT(0) : ((OUT(1) << VALIB) - OUT(1)));
+        static constexpr OUT STRLMask = (STRLB == 0 ? OUT(0) : ((OUT(1) << STRLB) - OUT(1) ));
+        static constexpr OUT CLKMask = (CLKB == 0 ? OUT(0) : ((OUT(1) << CLKB) - OUT(1) ));
+
+        template<typename VIt, typename STRLt, typename CLKt>
+        static constexpr OUT PackAnyT(VIt Value, VIt InverseValue, STRLt State, STRLt Relation, CLKt Clock)
+        {
+            OUT v = static_cast<OUT>(Value) & ValIMask;
+            OUT iv = static_cast<OUT>(InverseValue) & ValIMask;
+            OUT st = static_cast<OUT>(State) & STRLMask;
+            OUT rel = static_cast<OUT>(Relation) & STRLMask;
+            OUT clk = static_cast<OUT>(Clock) & CLKMask;
+
+            OUT outPAT = 0;
+            outPAT |= clk;
+            outPAT <<= (STRLB + STRLB) + (VALIB + VALIB);
+            outPAT |= (st << (STRLB + (VALIB + VALIB)));
+            outPAT |= (rel << (VALIB + VALIB));
+            outPAT |= (iv << VALIB);
+            outPAT |= v;
+            return outPAT;
+        }
+
+        template<typename VIt, typename STRLt, typename CLKt>
+        static constexpr void UnpackAnyT(OUT packed, VIt& value, VIt& inv, STRLt& st, STRLt& rel, CLKt& clk) noexcept
+        {
+
+            OUT cursor = packed;
+
+            value = static_cast<VIt>(cursor & ValIMask);
+            cursor >>= VALIB;
+
+            inv = static_cast<VIt>(cursor & ValIMask);
+            cursor >>= VALIB;
+
+            rel = static_cast<STRLt>(cursor & STRLMask);
+            cursor >>= STRLB;
+
+            st = static_cast<STRLt>(cursor & STRLMask);
+            cursor >>= STRLB;
+            
+            clk = static_cast<CLKt>(cursor & CLKMask);
+            
+        }
+    };
 
     struct uint4_tt
     {
